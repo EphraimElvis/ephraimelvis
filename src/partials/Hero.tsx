9d5dcc90c107841984f5264a202cc7a29756aa48
Ephraim Nyakpo Elvis <ephraim.elvis@yahoo.co.uk>
2023-08-19 01:25:16 +0600

    added project

---
+++ src/partials/Hero.tsx
@@ -0,0 +1,74 @@
+import {
+  GradientText,
+  HeroAvatar,
+  HeroSocial,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          <GradientText>Welcome !</GradientText> 👋
+        </>
+      }
+      description={
+        <div className="text-xl leading-8">
+          Ephraim is a skilled Kent-based
+          <a className="text-cyan-400 hover:underline" href="/">
+            {' '}
+            Frontend Developer
+          </a>{' '}
+          specializing in
+          <a className="text-cyan-400 hover:underline" href="/">
+            {' '}
+            React, Redux, Typescript and JavaScript.{' '}
+          </a>
+          With a strong eye for design and attention to detail, he creates
+          visually stunning and intuitive user interfaces. Committed to
+          excellence, Ephraim delivers exceptional web experiences that surpass
+          expectations.
+        </div>
+      }
+      avatar={
+        <img
+          className="h-[400px] w-full object-cover object-top"
+          style={{
+            borderRadius: '10px',
+          }}
+          src="/assets/profile/ephraim.jpg"
+          alt="Joy Shaheb Profile Picture"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <div className="mb-3 flex w-full justify-center gap-4 md:justify-start">
+          <a
+            href="https://github.com/ephraimelvis"
+            className="w-[32px]"
+            target="_blank"
+          >
+            <HeroSocial src="/assets/icons/github.svg" alt="Github icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ephraim-nyakpo-b7a67b212/"
+            className="w-[32px]"
+            target="_blank"
+          >
+            <HeroSocial src="/assets/icons/linkedin.svg" alt="Linkedin icon" />
+          </a>
+          <a
+            href="https://twitter.com/Ephraim70065681"
+            className="w-[32px]"
+            target="_blank"
+          >
+            <HeroSocial src="/assets/icons/twitter.svg" alt="Twitter icon" />
+          </a>
+        </div>
+      }
+    />
+  </Section>
+);
+
+export { Hero };
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+  site_name: 'Ephraim - Frontend Developer',
+  title: 'Ephraim - Frontend Developer',
+  description:
+    'Ephraim is a skilled Kent-based Frontend Developer specializing in React, Redux, Typescript and JavaScript. With a strong eye for design and attention to detail, he creates visually stunning and intuitive user interfaces. Committed to excellence, Ephraim delivers exceptional web experiences that surpass expectations.',
+  author: 'Ephraim Nyakpo',
+  locale_region: 'en-us',
+  locale: 'en',
+};
